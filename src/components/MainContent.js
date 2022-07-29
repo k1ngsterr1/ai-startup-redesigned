ec130d7ed2718576b62251256916cb148f46cffc
Ruslan <ruslanmakhmatov@gmail.com>
2022-07-29 18:53:33 +0600

    beta 1.2

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Brain from "../images/Brain.png";
+import Robot from "../images/ROBOT.png";
 
 const MainContent = () => {
   return (
+    // App Body Added
     <main className="main">
       <div className="main-header">
         <div className="square">
@@ -14,11 +16,17 @@ const MainContent = () => {
         Artificial Intelligence Is Already Among Us, Do You Want To{" "}
         <span className="blue-text">Try It By Yourself?</span>
       </p>
+      <p className="about-p-offer">
+        This Project Was Created In Order To Show the basics of the neural
+        networks and machine learning, through the chatting way. To put it more
+        simply, you can create your own aI and train it just with chatting{" "}
+      </p>
       <figure className="separator"></figure>
       <div className="buttons">
         <button className="start-btn">START</button>
         <button className="demo">DEMO</button>
       </div>
+      <img className="robot-picture" src={Robot}></img>
       {/* Offer And Quote Start */}
       {/* Buttons */}
       <img className="brain-picture" src={Brain}></img>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,12 @@ import "../styles/main_styles/main-styles.css";
 const NavBar = () => {
   return (
     <nav className="navigation">
+      <div className="logo-square">AI</div>
+      <div className="hamburger">
+        <div className="h-line-1"></div>
+        <div className="h-line-2"></div>
+        <div className="h-line-3"></div>
+      </div>
       <a className="home">HOME</a>
       <a className="about-project">ABOUT PROJECT</a>
       <a className="about-creator">ABOUT CREATOR</a>
